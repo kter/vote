2495904df05ce8d4f3cf862f25c519ce8857d097
kter <me@kter.jp>
2016-02-06 12:28:19 +0900

    generate top controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'top/index'
+
   resources :votes
   resources :speeches
   # The priority is based upon order of creation: first created -> highest priority.
