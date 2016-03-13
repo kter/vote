632b7f08b449c2b7f4585b2dedcc4ace7faaa725
kter <me@kter.jp>
2016-03-13 12:58:04 +0900

    fix validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,3 @@
 class Vote < ActiveRecord::Base
   validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
-  validates :comment, allow_nil: true, allow_blank: true
 end
