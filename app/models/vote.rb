9a465e50356ace5f06ec96b4fdde2fd9c29a59b6
kter <me@kter.jp>
2016-03-15 00:12:30 +0900

    presenterのpresence validationを追加

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,4 @@
 class Vote < ActiveRecord::Base
   validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3}
+  validates :presenter, presence: true
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @vote = Vote.new(comment: "comment", score: 3, presenter: "akari.mamiya", hold_date: "2016-03-05")
+  end
+
+  test "should be valid" do
+    assert @vote.valid?
+  end
+
+  test "presenter should be present" do
+    @vote.presenter = nil
+    assert_not @vote.valid?
+  end
+
 end
