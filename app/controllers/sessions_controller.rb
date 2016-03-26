ed465d001e8941e2c0a68387e46a3942758576f0
kter <me@kter.jp>
2016-03-26 17:26:06 +0900

    Add login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = 'You are now logged in as Administrator'
+      render root_path
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
