9713c4ecf927bacf930cff68f53a8e3a17602b3e
kter <me@kter.jp>
2016-03-26 21:57:52 +0900

    Add checkbox remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       flash.now[:success] = 'You are now logged in as Administrator'
       remember user
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
