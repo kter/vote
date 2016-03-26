64360339227a785d75db7b9ddc164464a0090e51
kter <me@kter.jp>
2016-03-26 21:15:05 +0900

    Delete remember digest when logged out

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,9 @@ class User < ActiveRecord::Base
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  # ユーザーログインを破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
