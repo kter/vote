b56b4da0ac9e357117f0e46207dd5ba864301260
kter <me@kter.jp>
2016-03-26 21:12:40 +0900

    Add feature to use cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       flash.now[:success] = 'You are now logged in as Administrator'
+      remember user
       log_in user
       redirect_to root_path
     else
