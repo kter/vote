9a0af882cc88e5c4ea9c1096d4518e82878f8206
kter <me@kter.jp>
2016-03-26 20:35:20 +0900

    Add remember_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326052612) do
+ActiveRecord::Schema.define(version: 20160326112848) do
 
   create_table "speeches", force: :cascade do |t|
     t.date     "hold_date"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160326052612) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
