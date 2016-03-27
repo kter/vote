dff4fe8ba86049b4115abd397596b2de5c4f0a03
kter <me@kter.jp>
2016-03-27 22:26:29 +0900

    Pass the test speeches

--- test/controllers/speeches_controller_test.rb
+++ test/controllers/speeches_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class SpeechesControllerTest < ActionController::TestCase
   setup do
     @speech = speeches("1st_speech")
+    @user = users(:miku)
   end
 
   test "should get index" do
@@ -12,11 +13,13 @@ class SpeechesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    log_in_as(@user)
     get :new
     assert_response :success
   end
 
   test "should create speech" do
+    log_in_as(@user)
     assert_difference('Speech.count') do
       post :create, speech: { hold_date: @speech.hold_date, presenter1: @speech.presenter1, presenter2: @speech.presenter2, theme1: @speech.theme1, theme2: @speech.theme2 }
     end
@@ -30,16 +33,19 @@ class SpeechesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    log_in_as(@user)
     get :edit, id: @speech
     assert_response :success
   end
 
   test "should update speech" do
+    log_in_as(@user)
     patch :update, id: @speech, speech: { hold_date: @speech.hold_date, presenter1: @speech.presenter1, presenter2: @speech.presenter2, theme1: @speech.theme1, theme2: @speech.theme2 }
     assert_redirected_to speech_path(assigns(:speech))
   end
 
   test "should destroy speech" do
+    log_in_as(@user)
     assert_difference('Speech.count', -1) do
       delete :destroy, id: @speech
     end
